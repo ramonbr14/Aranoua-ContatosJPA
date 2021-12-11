50ebe8dca1adc6b45e67cfcea7a66ed590eb5d02
ramonbr14 <ramonbr14@gmail.com>
2021-12-11 02:02:32 -0400

    Initial commit referente a aula 05

---
+++ src/main/java/br/edu/aranoua/contatos/AranouaContatosJpaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.aranoua.contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AranouaContatosJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AranouaContatosJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/aranoua/contatos/AranouaContatosJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.aranoua.contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AranouaContatosJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
